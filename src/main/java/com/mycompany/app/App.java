a50ea0b8ddeb0f7a8ab825e6629222938baf06f2
yudi-corgi <123s22@169.com>
2025-05-14 16:16:04 +0800

    style: comment.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
-        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
